728ef103725d4d764b3428df51107ccb0807fec7
Alfredo Bravo Cuero <yo@alfredobravocuero.co>
2023-03-23 00:37:19 +0100

    try function handleChange

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,6 +1,24 @@
+"use client";
+import { useState } from "react";
+
 function page() {
+
+  const [task,setTask] = useState()
+
+  const handleChange = (e) => {
+    console.log('Cambiado..')
+  }
+
   return (
-    <div>task form</div>
+    <form>
+      <input name="title" placeholder="Write a title"
+        onChange={handleChange}
+      />
+      <textarea name="desciption" placeholder="Write a description"
+         onChange={handleChange}
+      />
+      <button>Save</button>
+    </form>
   );
 }
 
